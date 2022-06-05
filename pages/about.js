4e6c9cc7495bfb2ec7cccf7f4ae2d7c80af835ae
curlycoders <105788102+curlycoders@users.noreply.github.com>
2022-06-05 15:03:18 +0530

    Updated About

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'; 
 import Header from '../components/header';
-import Footer from '../components/Footer';
+import Footer from '../components/footer';
 
 class About extends Component { 
     render() {
